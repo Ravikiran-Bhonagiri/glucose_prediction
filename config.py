a1e71fedd079423a6bc3387912b55bb6737e4c5c
Ravikiran Bhonagiri <bhonagiri.ravikiran@gmail.com>
2024-11-08 17:40:56 -0600

    Update config.py

--- config.py
+++ config.py
@@ -58,12 +58,12 @@ classification_config = {
 
 
 
-m_epochs = 1
+m_epochs = 100
 
 # Define the list of IDs
 # ids = ["MMCS0002", "MMCS0003", "MMCS0005", "MMCS0007", "MMCS0008", "MMCS0009", "MMCS0010", "MMCS0011", "MMCS0016"]
 
-ids = ["MMCS0002"]
+ids = ["MMCS0002", "MMCS0003", "MMCS0005", "MMCS0007"]
 
 # Define a dictionary to store results for each model and each ID
 #time_values = [12, 24, 48, 96]
