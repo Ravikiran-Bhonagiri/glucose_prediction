6d2571a471f6e12f288a2f5c94bc5698e6ee7f6d
Ravikiran Bhonagiri <bhonagiri.ravikiran@gmail.com>
2024-11-09 15:23:31 -0600

    Update regressor_models.py

--- regressor_models.py
+++ regressor_models.py
@@ -40,7 +40,7 @@ class LSTMModel(nn.Module):
     """
     def __init__(self, input_size, hidden_size, dropout=0.1):
         super(LSTMModel, self).__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, dropout, num_layers=2, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers=2, dropout=dropout, batch_first=True)
         self.fc1 = nn.Linear(hidden_size, 64)
         self.fc2 = nn.Linear(64, 1)  # Single output for regression
     
